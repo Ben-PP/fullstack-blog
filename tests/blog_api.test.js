4af3286677d22b4e12dd030d6407658de03a5919
Karel Parkkola <karel.parkkola@gmail.com>
2023-01-23 14:07:59 +0200

    Tests done

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -10,14 +10,12 @@ describe('api tests', () => {
 
   beforeEach(async () => {
     await Blog.deleteMany({})
-    console.log('cleared')
 
     const blogObjects = helper.initialBlogs
       .map(blog => new Blog(blog))
 
     const promiseArray = blogObjects.map(blog => blog.save())
     await Promise.all(promiseArray)
-    console.log('done')
   })
 
   test('blogs are returned as json', async () => {
@@ -30,6 +28,28 @@ describe('api tests', () => {
   test('there is correct amount of blogs', async () => {
     const response = await api.get('/api/blogs')
     expect(response.body).toHaveLength(6)
+  })
+
+  test('a valid note can be added ', async () => {
+    const newBlog = {
+      title: 'a new blog',
+      author: 'Ben',
+      url: '3djake.com',
+      likes: 6
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    const response = await api.get('/api/blogs')
+    const titles = response.body.map(r => r.title)
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+    expect(titles).toContain(
+      'a new blog'
+    )
   })
 
   afterAll(async () => {
